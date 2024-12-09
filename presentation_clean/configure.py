4ee9bc92911be53e34f4f4ef578ab619d97198e1
styczynski <piotr@styczynski.in>
2024-12-09 01:38:31 +0100

    feat: nice yaml parsing

--- presentation_clean/configure.py
+++ presentation_clean/configure.py
@@ -1,216 +1,6 @@
 import sys
 from datetime import datetime
-
-# © didlly AGPL-3.0 License - github.com/didlly
-
-def is_float(string: str) -> bool:
-    try:
-        float(string)
-        return True
-    except ValueError:
-        return False
-
-
-def is_integer(string: str) -> bool:
-    try:
-        int(string)
-        return True
-    except ValueError:
-        return False
-
-
-def load(path: str) -> dict:
-    with open(path, "r") as yaml:
-        levels = []
-        data = {}
-        indentation_str = ""
-
-        for line in yaml.readlines():
-            if line.replace(line.lstrip(), "") != "" and indentation_str == "":
-                indentation_str = line.replace(line.lstrip(), "").rstrip("\n")
-            if line.strip() == "":
-                continue
-            elif line.rstrip()[-1] == ":":
-                key = line.strip()[:-1]
-                quoteless = (
-                    is_float(key)
-                    or is_integer(key)
-                    or key == "True"
-                    or key == "False"
-                    or ("[" in key and "]" in key)
-                )
-
-                if len(line.replace(line.strip(), "")) // 2 < len(levels):
-                    if quoteless:
-                        levels[len(line.replace(line.strip(), "")) // 2] = f"[{key}]"
-                    else:
-                        levels[len(line.replace(line.strip(), "")) // 2] = f"['{key}']"
-                else:
-                    if quoteless:
-                        levels.append(f"[{line.strip()[:-1]}]")
-                    else:
-                        levels.append(f"['{line.strip()[:-1]}']")
-                if quoteless:
-                    exec(
-                        f"data{''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}]"
-                        + " = {}"
-                    )
-                else:
-                    exec(
-                        f"data{''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}']"
-                        + " = {}"
-                    )
-
-                continue
-
-            key = line.split(":")[0].strip()
-            value = ":".join(line.split(":")[1:]).strip()
-
-            if (
-                is_float(value)
-                or is_integer(value)
-                or value == "True"
-                or value == "False"
-                or ("[" in value and "]" in value)
-            ):
-                if (
-                    is_float(key)
-                    or is_integer(key)
-                    or key == "True"
-                    or key == "False"
-                    or ("[" in key and "]" in key)
-                ):
-                    exec(
-                        f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}] = {value}"
-                    )
-                else:
-                    exec(
-                        f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}'] = {value}"
-                    )
-            else:
-                if (
-                    is_float(key)
-                    or is_integer(key)
-                    or key == "True"
-                    or key == "False"
-                    or ("[" in key and "]" in key)
-                ):
-                    exec(
-                        f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}] = '{value}'"
-                    )
-                else:
-                    exec(
-                        f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}'] = '{value}'"
-                    )
-    return data
-
-
-def loads(yaml: str) -> dict:
-    levels = []
-    data = {}
-    indentation_str = ""
-
-    for line in yaml.split("\n"):
-        if line.replace(line.lstrip(), "") != "" and indentation_str == "":
-            indentation_str = line.replace(line.lstrip(), "")
-        if line.strip() == "":
-            continue
-        elif line.rstrip()[-1] == ":":
-            key = line.strip()[:-1]
-            quoteless = (
-                is_float(key)
-                or is_integer(key)
-                or key == "True"
-                or key == "False"
-                or ("[" in key and "]" in key)
-            )
-
-            if len(line.replace(line.strip(), "")) // 2 < len(levels):
-                if quoteless:
-                    levels[len(line.replace(line.strip(), "")) // 2] = f"[{key}]"
-                else:
-                    levels[len(line.replace(line.strip(), "")) // 2] = f"['{key}']"
-            else:
-                if quoteless:
-                    levels.append(f"[{line.strip()[:-1]}]")
-                else:
-                    levels.append(f"['{line.strip()[:-1]}']")
-            if quoteless:
-                exec(
-                    f"data{''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}]"
-                    + " = {}"
-                )
-            else:
-                exec(
-                    f"data{''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}']"
-                    + " = {}"
-                )
-
-            continue
-
-        key = line.split(":")[0].strip()
-        value = ":".join(line.split(":")[1:]).strip()
-
-        if (
-            is_float(value)
-            or is_integer(value)
-            or value == "True"
-            or value == "False"
-            or ("[" in value and "]" in value)
-        ):
-            if (
-                is_float(key)
-                or is_integer(key)
-                or key == "True"
-                or key == "False"
-                or ("[" in key and "]" in key)
-            ):
-                exec(
-                    f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}] = {value}"
-                )
-            else:
-                exec(
-                    f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}'] = {value}"
-                )
-        else:
-            if (
-                is_float(key)
-                or is_integer(key)
-                or key == "True"
-                or key == "False"
-                or ("[" in key and "]" in key)
-            ):
-                exec(
-                    f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}[{key}] = '{value}'"
-                )
-            else:
-                exec(
-                    f"data{'' if line == line.strip() else ''.join(str(i) for i in levels[:line.replace(line.lstrip(), '').count(indentation_str) if indentation_str != '' else 0])}['{key}'] = '{value}'"
-                )
-
-    return data
-
-
-def dumps(yaml: dict, indent="") -> str:
-    """A procedure which converts the dictionary passed to the procedure into it's yaml equivalent.
-
-    Args:
-        yaml (dict): The dictionary to be converted.
-
-    Returns:
-        data (str): The dictionary in yaml form.
-    """
-
-    data = ""
-
-    for key in yaml.keys():
-        if type(yaml[key]) == dict:
-            data += f"\n{indent}{key}:\n"
-            data += dumps(yaml[key], f"{indent}  ")
-        else:
-            data += f"{indent}{key}: {yaml[key]}\n"
-
-    return data
+import yaml
 
 # End YAML parser
 
@@ -230,17 +20,122 @@ def _merge(a: dict, b: dict, path=[]):
             a[key] = b[key]
     return a
 
+# Define digit mapping
+romanNumeralMap = (('M', 1000),
+                   ('CM', 900),
+                   ('D', 500),
+                   ('CD', 400),
+                   ('C', 100),
+                   ('XC', 90),
+                   ('L', 50),
+                   ('XL', 40),
+                   ('X', 10),
+                   ('IX', 9),
+                   ('V', 5),
+                   ('IV', 4),
+                   ('I', 1))
+
+_replacements = {
+    "&": r"\&",
+    "%": r"\%",
+    "$": r"\$",
+    "#": r"\#",
+    "_": r"\_",
+    "{": r"\{",
+    "}": r"\}",
+    "~": r"\textasciitilde{}",
+    "^": r"\^{}",
+    "\\": r"\textbackslash{}",
+    "\n": "\\newline%\n",
+    "-": r"{-}",
+    "\xA0": "~",  # Non-breaking space
+    "[": r"{[}",
+    "]": r"{]}",
+}
+_replacement_lookup = str.maketrans(_replacements)
+
+
+def escape(string):
+    """
+    Replace special characters with their equivalent LaTeX macros.
+
+    Args:
+        string (str): The string to process.
+
+    Returns:
+        str: The string with special characters replaced with macros.
+    """
+    return string.translate(_replacement_lookup)
+
+def toRoman(n):
+    """convert integer to Roman numeral"""
+    if not isinstance(n, int):
+        raise Exception("decimals cannot be converted")
+    if not (-1 < n < 5000):
+        raise Exception("number out of range (must be 0..4999)")
+
+    # special case
+    if n == 0:
+        return 'N'
+
+    result = ""
+    for numeral, integer in romanNumeralMap:
+        while n >= integer:
+            result += numeral
+            n -= integer
+    return result
+
+def _extract_paths(path, data):
+    all_paths = dict()
+    all_indices = []
+    if isinstance(data, list):
+        for (index, element) in enumerate(data):
+            all_paths.update(_extract_paths(f"{path}.{index}", element))
+            all_indices.append(str(index))
+        all_paths[path] = "{"+",".join([escape(idx) for idx in all_indices])+"}"
+    elif isinstance(data, dict):
+        for (key, element) in data.items():
+            all_paths.update(_extract_paths(f"{path}.{key}", element))
+            all_indices.append(str(key))
+        all_paths[path] = "{"+",".join([escape(idx) for idx in all_indices])+"}"
+    else:
+        all_paths[path] = f"{{{escape(str(data))}}}"
+    return all_paths
+
+def _convert(data):
+    tex = [
+        "\\makeatletter",
+        "\\def\\data#1{",
+    ]
+    # Extract paths
+    paths = list(enumerate(sorted(_extract_paths("", data).items())))
+    for (index, (path, value)) in paths:
+        path_value = path.removeprefix(".")
+        tex.append(f"\\ifnum\\pdfstrcmp{{#1}}{{{escape(path_value)}}}=0{value}\\else")
+    tex.append("??")
+    for _ in range(len(paths)):
+        tex.append("\\fi")
+    tex += [
+        "}",
+        "\\providecommand{\\foreachdata}[2]{",
+        "\\def\\v{\\data{#1}}",
+        "\\expanded{\\noexpand\\foreach\\noexpand#2 in \\v}",
+        "}",
+        "\\providecommand\\dataeq[2]{TT\\fi\\edef\\v{\\expanded{\\data{#1}}}\\ifnum\\pdfstrcmp\\v{#2}=0}",
+        "\\makeatother"
+    ]
+    return tex # [f"{line}" for line in tex]
+
+
 def configure(
     vars_file_name,
     meta_file_name,
     main_file_name,
     vars_str_defs,
 ):
-    vars_latex_defs = []
-    vars_latex_switches_defs = []
-
     defs = dict()
-    defs = load(meta_file_name)
+    with open(meta_file_name, 'r') as meta_file:
+        defs = yaml.safe_load(meta_file)
 
     for var_str in vars_str_defs:
         tokens = var_str.split("=")
@@ -261,21 +156,31 @@ def configure(
                 # Non-comment detected
                 break
     if len(extracted_main_meta_lines) > 0:
-        main_meta = loads("\n".join(extracted_main_meta_lines))
+        base_indent = len(extracted_main_meta_lines[0]) - len(extracted_main_meta_lines[0].lstrip())
+        yaml_str = "\n".join([line[base_indent:] for line in extracted_main_meta_lines])
+        print(yaml_str)
+        main_meta = yaml.safe_load(yaml_str)
         defs = _merge(defs, main_meta)
+        print(defs)
 
-    for var_name, var_value in defs.items():
-        if var_value in ["true", "false"]:
-            vars_latex_defs.append(f"\\newif\\ifbuild{var_name}\n")
-            vars_latex_switches_defs.append(f"\\build{var_name}{var_value}\n")
-        elif var_value.strip() in SPECIAL_VALUES:
-            vars_latex_defs.append(f"\\def\\build{var_name}{{{str(SPECIAL_VALUES[var_value.strip()]).strip()}}}\n")
-        else:
-            vars_latex_defs.append(f"\\def\\build{var_name}{{{str(var_value).strip()}}}\n")
+    # for var_name, var_value in defs.items():
+    #     if var_value in ["true", "false"]:
+    #         vars_latex_defs.append(f"\\newif\\ifbuild{var_name}\n")
+    #         vars_latex_switches_defs.append(f"\\build{var_name}{var_value}\n")
+    #     elif var_value.strip() in SPECIAL_VALUES:
+    #         vars_latex_defs.append(f"\\def\\build{var_name}{{{str(SPECIAL_VALUES[var_value.strip()]).strip()}}}\n")
+    #     else:
+    #         vars_latex_defs.append(f"\\def\\build{var_name}{{{str(var_value).strip()}}}\n")
 
     print(f"Output Latex config to: {vars_file_name}")    
     with open(vars_file_name, "w") as output_file:
-        output_file.writelines(vars_latex_defs + ["\n% Switches: \n"] + vars_latex_switches_defs)
+        output_file.writelines([
+            "%% Auto-generated variables\n",
+            "\n",
+            *_convert(defs),
+            "\n",
+            "%% End\n",
+        ])
 
 if __name__ == '__main__':
     configure(
